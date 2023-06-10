9e8a14848f3f98bb3ea6b75411bb1d27e60f809f
walter <walteroscarquintana456@gmail.com>
2023-06-09 22:30:41 -0300

    primer commit

---
+++ modelo/datospersona.php
@@ -0,0 +1,9 @@
+<?php
+
+class DatosPersona{
+    $Id=1;
+    $Nombre='Daniel';
+    $Apellido='Rodriguez';
+    $Documento='12456789';
+    $Direccion=
+}
---
+++ modelo/direccion.php
@@ -0,0 +1,11 @@
+<?php
+
+class Direccion{
+    $Id=1;
+    $Calle='Montevideo';
+    $Numero=34;
+    $Localidad='Rosario';
+    $Provincia=
+
+
+}
